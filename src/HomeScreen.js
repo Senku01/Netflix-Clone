677834852aeea9f12940ea9a20d4f856edf835c7
Senku01 <vigneshprakash2003@gmail.com>
2023-01-16 23:01:58 +0530

    Initial Commit

---
+++ src/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './HomeScreen.css'
+function HomeScreen() {
+  return (
+    <div className='homeScreen'>
+        HomeScreen
+    </div>
+  )
+}
+
+export default HomeScreen
